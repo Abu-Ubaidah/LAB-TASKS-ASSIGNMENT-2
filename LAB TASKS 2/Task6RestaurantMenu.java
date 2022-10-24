921895f841fbc883600ca04298be1a4e20ad5573
Abu Ubaidah <101874033+Abu-Ubaidah@users.noreply.github.com>
2022-10-24 16:58:56 +0500

    Update Task6RestaurantMenu.java

--- LAB TASKS 2/Task6RestaurantMenu.java
+++ LAB TASKS 2/Task6RestaurantMenu.java
@@ -1,37 +1,45 @@
 import java.util.*;
 class Task6RestaurantMenu{
     public static void main(String[] args) {
-        int sno;
+        int n,a;
 
         Scanner sc=new Scanner(System.in);
-
-        System.out.println("Welcome to Memon Restaurant, Enter the item Sno: to know the price.");
+        System.out.println("\n\n******************Welcome to Apna Dhaba******************\n\n");
+while(true){
+        System.out.println("\nMENU:\n");
         System.out.println("1.Zinger Burger");
         System.out.println("2.Large Pizza");
         System.out.println("3.Chicken Tikka");
         System.out.println("4.Seekh Kabab");
-        sno=sc.nextInt();
+        System.out.print("\nEnter the item no: to know the price : ");
 
-        switch(sno)
+        n = sc.nextInt();
+
+        switch(n)
         {
             case 1:
-            System.out.println("The Price for Zinger Burger is: 290/-");
+            System.out.println("\nThe Price of Zinger Burger is: 290/-");
             break;
 
             case 2:
-            System.out.println("The Price for Large Pizza is: 900/-");
+            System.out.println("\nThe Price of Large Pizza is: 900/-");
             break;
 
             case 3:
-            System.out.println("The Price for Chicken Tikka is: 400/-");
+            System.out.println("\nThe Price of Chicken Tikka is: 400/-");
             break;
 
             case 4:
-            System.out.println("The Price for Seekh Kabab is: 200/-");
+            System.out.println("\nThe Price of Seekh Kabab is: 200/-");
             break;
 
             default:
-            System.out.println("Please Enter a valid number");
+            System.out.println("\nPlease Enter a valid number");
+        }
+           System.out.println("\nPREES 1 IF YOU WANT TO GO BACK TO MENU OR PRESS ANY OTHER NUMBER TO EXIT"); 
+                a = sc.nextInt();
+            if(a!=1)
+             System.exit(0);              
         }
     }
 }
